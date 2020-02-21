46fcd615f9a12afd54781c4e0d19efb4661d2773
ROBHERN24 <60163659+ROBHERN24@users.noreply.github.com>
2020-02-20 20:49:49 -0600

    Add files via upload

---
+++ Programacion_Avanzada_HMRA-1/Programacion_Avanzada_HMRA/Main.java
@@ -0,0 +1,29 @@
+import Entidades.Persona;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //ejecutar el metodo constructor
+        Persona mario = new Persona(18, 170, 63.49);
+        System.out.println();
+        Persona marco = new Persona(13,152,53.45);
+        System.out.println();
+        Persona luis = new Persona(18,190,90.72);
+        System.out.println();
+        Persona francisco = new Persona(19,185,55.83);
+        System.out.println();
+        Persona alfredo = new Persona(80, 165, 73.49);
+        System.out.println();
+        Persona adrian = new Persona(17, 176, 53.54);
+        System.out.println();
+        Persona miguel = new Persona(19, 192, 100.785);
+        System.out.println();
+        Persona ximena = new Persona(20, 148, 62.1);
+        System.out.println();
+        Persona karime = new Persona(17, 174, 53.76);
+        System.out.println();
+        Persona alejandra = new Persona(25, 163, 70.36);
+        System.out.println();
+    }
+
+}
---
+++ Programacion_Avanzada_HMRA-1/Programacion_Avanzada_HMRA/Tarea/Persona.java
@@ -0,0 +1,16 @@
+package Tarea;
+
+public class Persona{
+
+        public int edad;
+        public int altura;
+        public double peso;
+
+        public Persona(int e, int a, double p){
+                edad = e;
+                altura = a;
+                peso = p;
+
+        }
+
+}
